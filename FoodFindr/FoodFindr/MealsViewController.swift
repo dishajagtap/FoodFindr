354d35bde412b02bd1cb25ab6a9ad1b7daf412d9
Sanchit Sinha <sinhasunny1979@gmail.com>
2019-03-13 23:21:30 -0700

    Created recipe class and got json from file

--- FoodFindr/FoodFindr/MealsViewController.swift
+++ FoodFindr/FoodFindr/MealsViewController.swift
@@ -7,13 +7,46 @@
 //
 
 import UIKit
+import SwiftyJSON
+
+class Recipe {
+    var uri: String
+    var label: String
+    var imageUrl: String
+    var calories: Double
+    init(hitJson: JSON) {
+        let recipe = hitJson["recipe"]
+        self.uri = recipe["uri"].string!
+        self.label = recipe["label"].string!
+        self.imageUrl = recipe["image"].string!
+        self.calories = recipe["calories"].double!
+    }
+}
 
 class MealsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("Path loading...")
         // Do any additional setup after loading the view.
+        guard let path = Bundle.main.path(forResource: "recipe_out_example", ofType: "json") else {
+            print("Failed")
+            return
+            
+        }
+        let url = URL(fileURLWithPath: path)
+        var recipeArr: [Recipe] = []
+        do {
+            let data = try Data(contentsOf: url)
+            let json = JSON(data)
+            let hits = json["hits"]
+            for h in hits {
+                recipeArr.append(Recipe(hitJson: h.1))
+            }
+        }
+        catch {
+            print(error)
+        }
     }
     
 
