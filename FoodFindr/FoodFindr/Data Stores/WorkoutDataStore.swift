82108eab42982073ec695b621b0949d13ad721f7
Disha Jagtap <dishaj555@gmail.com>
2019-03-16 01:07:45 -0700

    Retrieve burned calories with background refresh

--- FoodFindr/FoodFindr/Data Stores/WorkoutDataStore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WorkoutDataStore.swift
-//  FoodFindr
-//
-//  Created by Disha Jagtap on 3/11/19.
-//  Copyright © 2019 Disha Jagtap. All rights reserved.
-//
-
-import Foundation
--- FoodFindr/FoodFindr/WorkoutsViewController.swift
+++ FoodFindr/FoodFindr/WorkoutsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WorkoutsViewController.swift
 //  FoodFindr
 //
 //  Created by Disha Jagtap on 1/17/19.
@@ -14,42 +14,49 @@ class WorkoutsViewController: UIViewController {
         return .lightContent
     }
     
-    public let healthStore = HKHealthStore()
-    
-    func authorizeHealthKit() {
-        if HKHealthStore.isHealthDataAvailable() {
-            let infoToRead = Set([
-                HKSampleType.characteristicType(forIdentifier: .biologicalSex)!,
-                HKSampleType.characteristicType(forIdentifier: .dateOfBirth)!,
-                HKSampleType.quantityType(forIdentifier: .height)!,
-                HKSampleType.quantityType(forIdentifier: .bodyMass)!,
-                HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!,
-                HKSampleType.quantityType(forIdentifier: .appleExerciseTime)!,
-                HKSampleType.workoutType()
-                ])
-            
-            healthStore.requestAuthorization(toShare: infoToRead as? Set<HKSampleType>, read: infoToRead) { (success, error) in
-                if !success {
-                    // Handle the error here.
-                }
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         self.navigationItem.title = "Workouts"
-        authorizeHealthKit()
-        //this should get replaced later depending on the tab view
+        healthKitSetup()
+        fetchAndUpdateAEB()
+        setUpBackgroundDeliveryForAEB()
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         navigationController?.navigationBar.barStyle = .black
     }
     
-    @IBOutlet weak var caloriesBurnedLabel: UILabel!
-    @IBOutlet weak var exerciseMinutesLabel: UILabel!
-
-    @IBOutlet weak var caloriesBurnedText: UITextField!
-    @IBOutlet weak var exerciseMinutesText: UITextField!
+    func healthKitSetup() {
+        HealthKitSetupAssistant.authorizeHealthKit()
+    }
+    
+    func fetchAndUpdateAEB() {
+        ActiveEnergyBurnedDataStore.fetchActiveEnergyBurned()
+        updateAEBLabel()
+    }
+    
+    func updateAEBLabel() {
+        DispatchQueue.main.async {
+            let activeEnergyBurned = ActiveEnergyBurnedDataStore.getActiveEnergyBurned()
+            self.caloriesBurnedValue.text = "\(activeEnergyBurned) kCal"
+        }
+    }
+    
+    func setUpBackgroundDeliveryForAEB() {
+        let sampleType = HKSampleType.quantityType(forIdentifier: .activeEnergyBurned)!
+        let query = HKObserverQuery(sampleType: sampleType, predicate: nil) {
+            (query, completionHandler, error) in
+            debugPrint("observer query update handler called for type \(sampleType)")
+            completionHandler()
+            self.fetchAndUpdateAEB()
+        }
+        HealthKitSetupAssistant.healthStore.execute(query)
+        HealthKitSetupAssistant.healthStore.enableBackgroundDelivery(for: sampleType, frequency: .immediate) { (complete, error) in
+            debugPrint("enableBackgroundDeliveryForType handler called for \(sampleType) - success: \(complete)")
+        }
+    }
+    
+    @IBOutlet public weak var caloriesBurnedLabel: UILabel!
+    @IBOutlet public weak var caloriesBurnedValue: UILabel!
 }
